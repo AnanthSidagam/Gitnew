e44571f233a88fad3944c20b8daa3e82c7cbc8de
Ananth <ananth.sidgam@gmail.com>
2022-02-12 21:43:12 +0530

    first commit

---
+++ src/main/java/mavenananth/App.java
@@ -0,0 +1,13 @@
+package mavenananth;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/mavenananth/RESTAPiTest.java
@@ -0,0 +1,18 @@
+package mavenananth;
+
+import org.testng.annotations.Test;
+
+public class RESTAPiTest {
+	@Test
+	public void postjira()
+	{
+		System.out.println("postjira");
+	}
+
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+	}
+
+}
